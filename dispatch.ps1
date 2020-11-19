2a2dabd77fb93cbb54af85f41d04bc264f4173a3
xa0082249956 <576810415@qq.com>
2020-11-19 23:19:53 +0800

    add action

---
+++ dispatch.ps1
@@ -0,0 +1,19 @@
+#! /usr/bin/env pwsh
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $Token,
+    [Parameter(Mandatory)]
+    [string]
+    $Repository,
+    [Parameter(Mandatory)]
+    [string]
+    $Workflow
+)
+
+Invoke-RestMethod `
+    -Method Post `
+    -Headers @{Accept='application/vnd.github.v3+json'; `
+               Authorization="Bearer $Token"} `
+    -Body '{"ref": "master"}' `
+    -Uri "https://api.github.com/repos/$Repository/actions/workflows/$Workflow/dispatches" `
