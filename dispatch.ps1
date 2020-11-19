24f1013750f55e54097d157d1b12b5081729b523
xa0082249956 <576810415@qq.com>
2020-11-19 23:45:19 +0800

    add action

--- dispatch.ps1
+++ dispatch.ps1
@@ -16,4 +16,4 @@ Invoke-RestMethod `
     -Headers @{Accept='application/vnd.github.v3+json'; `
                Authorization="Bearer $Token"} `
     -Body '{"ref": "master"}' `
-    -Uri "https://api.github.com/repos/$Repository/actions/workflows/$Workflow/dispatches" `
+    -Uri "https://api.github.com/repos/$Repository/actions/workflows/$Workflow/dispatches"
